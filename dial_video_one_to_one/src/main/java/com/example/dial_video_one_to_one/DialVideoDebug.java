f398d5121b47d1aa2f00327f24de3d77d51449f0
SandiChai <69144919+SandiChai@users.noreply.github.com>
2020-08-03 16:45:21 +0700

    First Commit

---
+++ dial_video_one_to_one/src/main/java/com/example/dial_video_one_to_one/DialVideoDebug.java
@@ -0,0 +1,11 @@
+package com.example.dial_video_one_to_one;
+
+import android.util.Log;
+
+public class DialVideoDebug {
+    private static final String TAG = "SUPER_AWESOME_APP";
+
+    public static void d(String message){
+        Log.d(TAG, message);
+    }
+}
